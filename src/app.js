c25630306634b27a569712bb342be5f35cf70a38
anshuman-codes <anshuman.sharma4801@gmail.com>
2022-01-10 23:21:19 +0530

    some patch2

--- src/app.js
+++ src/app.js
@@ -1,12 +1,14 @@
 require("./db/mongoose.js");
 const express = require("express");
-const userRouter = require("./routers/user");
-const taskRouter = require("./routers/task");
 const cors = require("cors");
 
 const app = express();
 
 app.use(cors());
+
+const userRouter = require("./routers/user");
+const taskRouter = require("./routers/task");
+
 app.use(express.json()); // Automatically parses incoming JSON as an object for our use
 app.use(userRouter);
 app.use(taskRouter);
